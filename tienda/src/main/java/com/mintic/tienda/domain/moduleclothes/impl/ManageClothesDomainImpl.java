d728f0b62b3c4a4b4c397f924ea7f4e9cff68221
esneider-ies- <david.loaiza@iesonline.com.co>
2022-09-21 13:54:35 -0500

    feat: Se muestra lista de prendas y se agrega prenda usando plantillas

--- tienda/src/main/java/com/mintic/tienda/domain/moduleclothes/impl/ManageClothesDomainImpl.java
+++ tienda/src/main/java/com/mintic/tienda/domain/moduleclothes/impl/ManageClothesDomainImpl.java
@@ -2,13 +2,18 @@ package com.mintic.tienda.domain.moduleclothes.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.mintic.tienda.domain.moduleclothes.IManageClothesDomain;
 import com.mintic.tienda.service.DTO.PrendaDTO;
 import com.mintic.tienda.service.moduleclothes.IManageClothesService;
 
 
+@Component
 public class ManageClothesDomainImpl implements IManageClothesDomain{
 
+   @Autowired
    private IManageClothesService iManageClothesService;
 
    public PrendaDTO savePrenda(PrendaDTO prendaDTO) {
