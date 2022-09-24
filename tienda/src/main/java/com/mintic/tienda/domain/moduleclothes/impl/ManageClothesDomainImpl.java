6a657d40c64c8bba6d47915945c982aff44b34cc
EsneiderLoaiza <esneiderlvs@gmail.com>
2022-09-24 14:53:37 -0500

    config: Se deja configuración del proyecto por defecto

--- tienda/src/main/java/com/mintic/tienda/domain/moduleclothes/impl/ManageClothesDomainImpl.java
+++ tienda/src/main/java/com/mintic/tienda/domain/moduleclothes/impl/ManageClothesDomainImpl.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.mintic.tienda.domain.moduleclothes.IManageClothesDomain;
-import com.mintic.tienda.entity.Prenda;
 import com.mintic.tienda.service.DTO.PrendaDTO;
 import com.mintic.tienda.service.moduleclothes.IManageClothesService;
 
@@ -24,8 +23,6 @@ public class ManageClothesDomainImpl implements IManageClothesDomain{
    public List<PrendaDTO> getAllPrendas() {
     return iManageClothesService.getAllPrendas();
    }
-
-   
 
    public PrendaDTO getOnePrenda(Long idPrenda) {
     return iManageClothesService.getOnePrenda(idPrenda);
--- tienda/src/main/java/com/mintic/tienda/service/moduleclothes/IManageClothesService.java
+++ tienda/src/main/java/com/mintic/tienda/service/moduleclothes/IManageClothesService.java
@@ -2,7 +2,6 @@ package com.mintic.tienda.service.moduleclothes;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
 
 import com.mintic.tienda.domain.moduleclothes.IManageClothesDomain;
 import com.mintic.tienda.service.DTO.PrendaDTO;
@@ -10,7 +9,6 @@ import com.mintic.tienda.service.DTO.PrendaDTO;
 public interface IManageClothesService extends IManageClothesDomain {
     
    PrendaDTO savePrenda(PrendaDTO prendaDTO);
-   
    
    List<PrendaDTO> getAllPrendas();
 
